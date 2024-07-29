00de00b17a528576c0aafba9ddddd080b48463c1
motconkien <hynuiux@gmail.com>
2024-07-30 00:03:26 +0900

    Updated

--- Day50/main.py
+++ Day50/main.py
@@ -8,89 +8,85 @@ import time
 
 URL = "https://tinder.com"
 fb_email = "thanhhuyenclinic@gmail.com"
-fb_pass = "huyexxxx"
+fb_pass = "huyen@20897"
+
+# com: 
+#   Log in:
 driver = webdriver.Safari()
+driver.set_window_size(800,600)
 driver.get(URL)
 
 # Wait for the page to load
 time.sleep(5)
 
-wait = WebDriverWait(driver, 60)
+driver.implicitly_wait(10)
 
+accept_btn = driver.find_element(By.XPATH, "//div[contains(text(), 'I accept')]/ancestor::button")
+if accept_btn:
+    accept_btn.click()
+    print("Clicked accept_btn")
 
-def close_popup(): 
-    try:
-        accept_btn = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'I accept')]/ancestor::button")))
-        if accept_btn:
-            accept_btn.click()
-            print("Click accept btn")
-    except NoSuchElementException:
-        print("Not found")
+login_btn = driver.find_element(By.XPATH, "//div[@class = 'Expand']//button[.//div[contains(text(),'Log in')]]")
+if login_btn:
+    login_btn.click()
+    print("Clicked login_btn")
 
+# Wait for login more
+time.sleep(5)
 
+more_btn = None
 try:
-    close_popup()
-    login_btn = wait.until(
-        EC.element_to_be_clickable((
-            By.XPATH,"//div[@class = 'Expand Pos(r)']//button[.//div[contains(text(),'Log in')]]")
-        )
-    )
-
+  more_btn = driver.find_element(By.XPATH, "//div[@class = 'Mt(a)']//button[contains(text(),'More Options')]")
 except NoSuchElementException:
-    print("Not found")
-else:
-    if login_btn:
-        login_btn.click()
-        print("Click log in")
+  print("Mpre options is not found")
 
-        try:
-            fb_btn = wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH,"//div[@class = 'Mt(a)']//button[.//div[contains(text(), 'Log in with Facebook')]]")))
-            if fb_btn:
-                fb_btn.click()
-                print("found")
-        except NoSuchElementException:
-            print("Not found")
-        else:
-            time.sleep(10)
-            base_window = driver.window_handles[0]
-            fb_window = driver.window_handles[1]
-            driver.switch_to.window(fb_window)
-            print(driver.title)
+if more_btn:
+    more_btn.click()
+    print("Clicked more_btn")
 
-            email = driver.find_element(By.XPATH, '//*[@id="email"]')
-            password = driver.find_element(By.XPATH, '//*[@id="pass"]')
-            # btn = driver.find_element(By.XPATH,'//*[@id="buttons"]')
-            email.send_keys(fb_email)
-            password.send_keys(fb_pass)
-            password.send_keys(Keys.ENTER)
-            # btn.click()
 
-            driver.switch_to.window(base_window)
-            print(driver.title)
+# Wait for clicked more
+time.sleep(5)
 
-            time.sleep(5)
-            allow_location = wait.until(
-                EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Allow' and @data-testid='allow']"))
-            )
-            allow_location.click()
+fb_btn = None
+try:
+  fb_btn = driver.find_element(By.XPATH, "//div[@class = 'Mt(a)']//button[@aria-label='Log in with Facebook']")
+except NoSuchElementException:
+  print("fb_btn is not found")
 
-            noti_btn = driver.find_element(By.XPATH,"//button[@aria-label='I'll miss out' and @data-testid='decline']")
-            noti_btn.click()
+if fb_btn:
+    fb_btn.click()
+    print(fb_btn)
+    print("Clicked fb_btn")
 
-            # for n in range(10):
-            #     time.sleep(2)
 
-            #     try:
-            #         print("Called")
-            #         like_btn = wait.until(
-            #                         EC.element_to_be_clickable((
-            #                             By.XPATH,'(//div[contains(@class, "Pos(a) B(0) Iso(i) W(100%) Start(0) End(0)")]/div/div/button)[4]')
-            #                     )
-            #                 )
-            #         like_btn.click()
-            #     except NoSuchElementException:
-            #         print("not found")
+fbx_btn = None
+try:
+  fbx_btn = driver.find_element(By.XPATH, "//div[@class = 'Mt(a)']//button[@aria-label='Login with Facebook']")
+except NoSuchElementException:
+  print("fbx_btn is not found")
 
+if fbx_btn:
+    fbx_btn.click()
+    print("Clicked fbx_btn")
+time.sleep(10)
+base_window = driver.window_handles[0]
+fb_window = driver.window_handles[1]
+driver.switch_to.window(fb_window)
+print(driver.title) 
+
+#switch window
+print(driver.window_handles)
+base_window = driver.window_handles[0]
+fb_window = driver.window_handles[1]
+driver.switch_to.window(fb_window)
+print(driver.title)
+time.sleep(10)
+email = driver.find_element(By.XPATH,'//input[@id="email"]')
+password = driver.find_element(By.XPATH,'//input[@id="pass"]')
+btn_login = driver.find_element(By.XPATH,'//input[@name="login"]')
+print("Sending keys")
+email.send_keys(fb_email)
+password.send_keys(fb_pass)
+btn_login.click()
+driver.switch_to.window(base_window)
